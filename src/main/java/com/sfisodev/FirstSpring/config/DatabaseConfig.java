2f20abff44b0b2343775e686b58666aeebdf19b6
SfisoMawila <sfiso@thedigitalacademy.co.za>
2024-12-13 10:06:39 +0200

    first commit

---
+++ src/main/java/com/sfisodev/FirstSpring/config/DatabaseConfig.java
@@ -0,0 +1,17 @@
+package com.sfisodev.FirstSpring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(final DataSource source){
+        return new JdbcTemplate(source);
+
+    }
+}
